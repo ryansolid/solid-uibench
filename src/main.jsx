2895e1845f5078f9156545e9db642d2baab85b0d
Ryan Carniato <ryansolid@gmail.com>
2019-04-16 21:14:14 -0700

    update deps

--- src/main.jsx
+++ src/main.jsx
@@ -51,7 +51,7 @@ const Main = ({data}) => {
   return <div class="Main">{section}</div>
 }
 
-uibench.init('Solid', '0.4.2');
+uibench.init('Solid', '0.5.0');
 const [state, setState] = createState();
 createRoot(() => document.querySelector('#App').appendChild(<Main data={state} />))
 
